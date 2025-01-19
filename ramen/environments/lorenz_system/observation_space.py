e1f84ac000f9fb19b31e91a4fc8fa25da695a98f
SlowWave <chrs.borgia@gmail.com>
2025-01-19 21:09:19 +0100

    updated "ObservationSpaceModel"

--- ramen/environments/lorenz_system/observation_space.py
+++ ramen/environments/lorenz_system/observation_space.py
@@ -14,8 +14,13 @@ class ObservationSpaceModel():
 
         # define mapping dictionary
         self.observation_model_map = {
-            "1": self._model_1,
-            "2": self._model_2,
+            "1": self._observation_model_1,
+            "2": self._observation_model_2,
+        }
+        
+        self.observation_map = {
+            "1": self._observation_1,
+            "2": self._observation_2,
         }
 
         self.model_id = str(CFG["gymnasium"]["observation_space"]["model_id"])
@@ -27,20 +32,30 @@ class ObservationSpaceModel():
         
         return self.observation_model_map[self.model_id]()
 
-    def _model_1(self):
+    def get_observation(self):
+
+        if self.model_id not in self.observation_model_map:
+            raise ValueError(f"Unsupported model_id: {self.model_id} for obsevation space")
+        
+        return self.observation_model_map[self.model_id]()
+
+    def _observation_model_1(self):
 
         # define observation space limits
         observation_limit = np.array(
             [
-                1,                          # mrp tracking error [0]
-                1,                          # mrp tracking error [1]
-                1,                          # mrp tracking error [2]
-                np.finfo(np.float32).max,   # omega tracking error [0]
-                np.finfo(np.float32).max,   # omega tracking error [1]
-                np.finfo(np.float32).max,   # omega tracking error [2]
-                np.finfo(np.float32).max,   # feedback control signal [0]
-                np.finfo(np.float32).max,   # feedback control signal [1]
-                np.finfo(np.float32).max,   # feedback control signal [2]
+                np.finfo(np.float32).max,   # state [0]
+                np.finfo(np.float32).max,   # state [1]
+                np.finfo(np.float32).max,   # state [2]
+                np.finfo(np.float32).max,   # state difference [0]
+                np.finfo(np.float32).max,   # state difference [1]
+                np.finfo(np.float32).max,   # state difference [2]
+                np.finfo(np.float32).max,   # distance from eq point 1 [0]
+                np.finfo(np.float32).max,   # distance from eq point 1 [1]
+                np.finfo(np.float32).max,   # distance from eq point 1 [2]
+                np.finfo(np.float32).max,   # distance from eq point 2 [0]
+                np.finfo(np.float32).max,   # distance from eq point 2 [1]
+                np.finfo(np.float32).max,   # distance from eq point 2 [2]
             ],
             dtype=np.float32,
         )
@@ -54,7 +69,12 @@ class ObservationSpaceModel():
 
         return observation_space
 
-    def _model_2(self):
+    def _observation_1(self, state):
+
+        obs = state
+        return obs
+
+    def _observation_model_2(self):
 
         # define observation space limits
         observation_limit = np.array(
@@ -83,3 +103,9 @@ class ObservationSpaceModel():
         )
 
         return observation_space
+    
+    
+    def _observation_2(self, state):
+        
+        obs = state
+        return obs
