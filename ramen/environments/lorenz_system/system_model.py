6f26186a2d652cd05f07fdbd2381549cd9f28b65
SlowWave <chrs.borgia@gmail.com>
2025-01-21 20:00:39 +0100

    added equilibrium points computation

--- ramen/environments/lorenz_system/system_model.py
+++ ramen/environments/lorenz_system/system_model.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import tomli
+import math
 
 # add parent directory to "sys.path" to import modules from that path
 sys.path.append(os.path.normpath(os.path.dirname(__file__) + os.sep + os.pardir))
 
-from modules.dynamic_system import DynamicSystem
+from modules.dynamic_system_copy import DynamicSystem
 
 # get config data
 with open(os.path.join(os.path.dirname(__file__),"config.toml"), "rb") as config_file:
@@ -26,10 +27,12 @@ class LorenzSystem(DynamicSystem):
         self.state_boundaries = CFG["dynamic_system"]["state_boundaries"]
         self.input_boundaries = CFG["dynamic_system"]["input_boundaries"]
         self.tag = "LorenzSystem"
-           
+        
         # simulation attributes
         self.integration_step = CFG["dynamic_system"]["simulation"]["integration_step"]
 
+        # set equilibrium points
+        self.equilibrium_points = self._get_equlibrium_points()
 
     def ode(self, t, x, u):
         """
@@ -52,7 +55,22 @@ class LorenzSystem(DynamicSystem):
 
         return x_dot
     
+    def _get_equlibrium_points(self):
+        
+        eq_1 = [0.0, 0.0, 0.0]
+        eq_2 = [
+            math.sqrt(self.beta * (self.rho - 1.0)),
+            math.sqrt(self.beta * (self.rho - 1.0)),
+            self.rho - 1.0
+        ]
+        eq_3 = [
+            -math.sqrt(self.beta * (self.rho - 1.0)),
+            -math.sqrt(self.beta * (self.rho - 1.0)),
+            self.rho - 1.0
+        ]
+        
+        return [eq_1, eq_2, eq_3]
     
 if __name__ == "__main__":
     lorenz_system = LorenzSystem()
-    lorenz_system.simulate_system(100, use_inputs=False)
+    lorenz_system.simulate_system(100)
